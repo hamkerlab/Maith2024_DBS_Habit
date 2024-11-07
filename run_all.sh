f960097f38e3d1b155bd9255ab43b7104f403e0f
olmai <oli_maith@gmx.de>
2024-11-07 15:10:27 +0100

    run_all: remove annarchy_folders in between

--- run_all.sh
+++ run_all.sh
@@ -15,13 +15,19 @@ fi
 python patient_data.py
 # run run_simulation using given cores in mode 0 --> get_simulation_data
 python run_simulation.py ${1} 0
+rm -rf annarchy_folders/
 # run run_simulation using given cores in mode 1 --> get_activity_change_data
 python run_simulation.py ${1} 1
+rm -rf annarchy_folders/
 # run run_simulation using given cores in mode 2 --> get_dbs_parameter_data
 python run_simulation.py ${1} 2
+rm -rf annarchy_folders/
 # run run_simulation using given cores in mode 3 --> get_load_simulate_data
 python run_simulation.py ${1} 3
+rm -rf annarchy_folders/
 # run run_simulation using given cores in mode 4 --> run_statistic
 python run_simulation.py ${1} 4
+rm -rf annarchy_folders/
 # run run_simulation using given cores in mode 5 --> plot_figures
 python run_simulation.py ${1} 5
+rm -rf annarchy_folders/
